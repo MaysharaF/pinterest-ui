d97e38bc5adb804794a58e6a5b7f787992a07394
MaysharaF <mayshara@alunos.utfpr.edu.br>
2024-02-26 21:03:30 -0300

    feat: adicionando skeleton

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { StyleSheet, View, useWindowDimensions } from "react-native";
 
+import { Skeleton } from "moti/skeleton";
+
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -15,6 +17,12 @@ export default function Splash() {
   const logoPositionY = useSharedValue(0);
 
   const dimensions = useWindowDimensions();
+
+  const skeletonColors = [
+    theme.colors.gray[600],
+    theme.colors.gray[700],
+    theme.colors.gray[600],
+  ];
 
   const logoAnimatedStyles = useAnimatedStyle(() => ({
     transform: [
@@ -38,6 +46,18 @@ export default function Splash() {
     );
   }
 
+  function filters() {
+    return Array.from({ length: 10 }).map((_, index) => (
+      <Skeleton
+        key={index}
+        width={60}
+        height={26}
+        radius={6}
+        colors={skeletonColors}
+      />
+    ));
+  }
+
   function boxes(column: "right" | "left") {
     const rest = column === "left" ? 0 : 1;
 
@@ -47,10 +67,9 @@ export default function Splash() {
         const height = index % 2 === (column === "left" ? 0 : 1) ? 200 : 300;
 
         return (
-          <Animated.View
-            key={index}
-            style={[styles.box]}
-          ></Animated.View>
+          <Animated.View key={index} style={[styles.box]}>
+            <Skeleton colors={skeletonColors} width="100%" height={height} />
+          </Animated.View>
         );
       });
   }
@@ -66,6 +85,8 @@ export default function Splash() {
         style={[styles.logo, logoAnimatedStyles]}
       />
 
+      <View style={styles.header}>{filters()}</View>
+
       <View style={styles.boxes}>
         <View style={styles.column}>{boxes("left")}</View>
         <View style={styles.column}>{boxes("right")}</View>
@@ -80,24 +101,30 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.black,
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 12
   },
   logo: {
     width: 64,
     height: 64,
   },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 16,
+    paddingBottom: 12,
+  },
   boxes: {
     flex: 1,
     width: "100%",
     flexDirection: "row",
-    gap: 12
+    gap: 12,
   },
   box: {
     width: "100%",
     borderRadius: 16,
-
   },
   column: {
     flex: 1,
-    gap: 12
+    gap: 12,
   },
 });
